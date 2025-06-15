d57a704e186eca47f9d318e89d01b81540c2b914
Nazmul Hassan <nazmulnhb007@yahoo.com>
2025-06-15 20:54:18 +0600

    updated cors

--- src/app/configs/cors.ts
+++ src/app/configs/cors.ts
@@ -1,26 +1,26 @@
 import { type CorsOptions } from 'cors';
 
 export const corsOptions: CorsOptions = {
-	// origin: (origin, callback) => {
-	// 	const allowedOrigins = [
-	// 		/^http:\/\/localhost:\d+$/,
-	// 		/^http:\/\/192\.168\.184\.74:\d+$/,
-	// 		'https://bill-buddy-nhb.vercel.app',
-	// 	];
+	origin: (origin, callback) => {
+		const allowedOrigins = [
+			/^http:\/\/localhost:\d+$/,
+			/^http:\/\/192\.168\.224\.74:\d+$/,
+			/^http:\/\/192\.168\.0\.6:\d+$/,
+		];
 
-	// 	if (
-	// 		!origin ||
-	// 		allowedOrigins.some((pattern) =>
-	// 			typeof pattern === 'string' ?
-	// 				pattern === origin
-	// 			:	pattern.test(origin),
-	// 		)
-	// 	) {
-	// 		callback(null, true);
-	// 	} else {
-	// 		callback(new Error('Not allowed by CORS'));
-	// 	}
-	// },
-	origin: '*',
+		if (
+			!origin ||
+			allowedOrigins.some((pattern) =>
+				typeof pattern === 'string' ?
+					pattern === origin
+				:	pattern.test(origin),
+			)
+		) {
+			callback(null, true);
+		} else {
+			callback(new Error('Not allowed by CORS'));
+		}
+	},
+	// origin: '*',
 	credentials: true,
 };
