4ae95d6b115ac6f27dd63081069f8a3095839876
Nazmul Hassan <nazmulnhb007@yahoo.com>
2025-06-20 22:21:31 +0600

    added customm logger middleware

--- src/app.ts
+++ src/app.ts
@@ -1,17 +1,20 @@
 import cookieParser from 'cookie-parser';
 import cors from 'cors';
-import type { Application, Request, Response } from 'express';
+import type { Application } from 'express';
 import express from 'express';
 import { corsOptions } from './app/configs/cors';
 import {
 	catchAllErrors,
 	handleRouteNotFound,
 } from './app/middlewares/errorHandlers';
+import { requestLogger } from './app/middlewares/requestLogger';
 import router from './app/routes';
 import sendResponse from './app/utilities/sendResponse';
 
 // * Create an Express App
 const app: Application = express();
+
+app.set('trust proxy', true);
 
 // * Respect CORS Policy
 app.use(cors(corsOptions));
@@ -20,9 +23,11 @@ app.use(cors(corsOptions));
 app.use(cookieParser());
 // * Use JSON Parser
 app.use(express.json());
+// * Use custom logger
+app.use(requestLogger);
 
 // * Root/Test Route
-app.get(['/', '/api'], (_req: Request, res: Response) => {
+app.get(['/', '/api'], (_req, res) => {
 	sendResponse(res, 'N/A', 'OK', null, 'Hourly Server is Running! 🏃');
 });
 
